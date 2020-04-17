f6316dbb9bb96d5e9f6fa215d80169dbce95e06d
maustinstar <maustinstar@gmail.com>
2020-04-16 22:50:26 -0500

    remove executable

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "SimulatorControl",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .executable(name: "simscript", targets: ["SimulatorControl"]),
         .library(
             name: "SimulatorControl",
             targets: ["SimulatorControl"]),
