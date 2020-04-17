7cf6cc22b5de4d9535d31dda207a83a6d96604d9
maustinstar <maustinstar@gmail.com>
2020-04-16 22:16:11 -0500

    executable

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "SimulatorControl",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(name: "simscript", targets: ["SimulatorControl"]),
         .library(
             name: "SimulatorControl",
             targets: ["SimulatorControl"]),
